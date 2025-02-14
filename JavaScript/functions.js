1bd0322ce70e21a986f44eca64e2309da9201c36
MITHUSHIYA <sherinmithushiya@gmail.com>
2025-02-14 19:45:27 +0530

    Js:: Functions

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -4,3 +4,19 @@ function testFunction(){
 testFunction();
 
 //TASK :: practise remaining three types of function based on parameter and argument
+
+function hasArgument(num){
+    console.log("This has a argument",num);
+}
+hasArgument(3);
+
+function hasReturn(){
+    return "Hello";
+}
+str = hasReturn();
+console.log(str);
+
+function ReturnAndArgument(x , y){
+    return x*y;    
+}
+console.log(ReturnAndArgument(2,4));
