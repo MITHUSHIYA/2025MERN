0f2437091a9430d29ebe61f37aaa60072231b7e1
MITHUSHIYA <sherinmithushiya@gmail.com>
2025-02-19 19:03:58 +0530

    Hooks: useState & useEffect

--- sjitFrontend/src/App.jsx
+++ sjitFrontend/src/App.jsx
@@ -4,6 +4,7 @@ import Contact from "./components/FunctionalComponents/Contact";
 import Gallery from "./components/FunctionalComponents/Gallery";
 import Signup from "./components/FunctionalComponents/Signup";
 import Navbar from "./components/FunctionalComponents/Navbar";
+import UseEffect from "./components/FunctionalComponents/Hooks/UseEffect"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
             <Route path="/gallery" element={<Gallery />}></Route>
             <Route path="/contact" element={<Contact />}></Route>
             <Route path="/signup" element={<Signup />}></Route>
+            <Route path="/use-effect" element={<UseEffect />}></Route>
           </Routes>
         </BrowserRouter>
       </main>
--- sjitFrontend/src/components/FunctionalComponents/Navbar.jsx
+++ sjitFrontend/src/components/FunctionalComponents/Navbar.jsx
@@ -1,15 +1,28 @@
+import { Link } from "react-router-dom";
 import "../css/Navbar.css";
 
 const Navbar = () => {
   return (
     <header>
       <nav>
-        <li><Link></Link></li>
-        <li>Home</li>
-        <li>About</li>
-        <li>Gallery</li>
-        <li>Contact</li>
-        <li>Signup</li>
+        <li>
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/gallery">Gallery</Link>
+        </li>
+        <div>
+          <span>Hooks</span>
+          <ol>
+            <li><Link to='/use-state'>useState</Link></li>
+            <li><Link to='/use-effect'>useEffect</Link></li>
+          </ol>
+        </div>
+        <li><Link to={'/contact'}>Contact</Link></li>
+        <li><Link to = '/signup'>Signup</Link></li>
       </nav>
     </header>
   );
