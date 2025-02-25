fd307a085d5fd381cb2d6aacfdfef289cc915366
MITHUSHIYA <sherinmithushiya@gmail.com>
2025-02-25 13:45:55 +0530

    Backend to MongoDB integration

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,16 +1,65 @@
 const express = require("express");
+const mdb = require("mongoose"); /* mdb-MongoDB */
+const dotenv = require("dotenv"); /* This is to access the MONGODB_URL path from .env file we need to install "npm i dotenv" to acess .env*/
+const Signup = require("./models/signupSchema");
 
 const app = express(); /* Crusial line in creating a server */
 
+app.use(express.json());
+const PORT = 3001;
+dotenv.config();
+
+
+
+console.log(
+  process.env.MONGODB_URL
+); /* alternate to MONGODB_URL is "mongoose://127.0.0:27017//MERN" here '127.0.0' is the ip address */
+mdb
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("MDB Connection Successful");
+  })
+  .catch((err) => {
+    console.log("Check your connection String:", err);
+  });
+
+
+
 app.get("/", (req, res) => {
-  res.send("<h1>Welcome to Backend Server.</h1>");  /* is the server continuous (or) static server ? */
+  res.send(
+    "<h1>Welcome to Backend Server!!</h1>"
+  ); /* is the server continuous (or) static server ? */
 });
 
-app.get("/static",(req,res)=>{
-  res.sendFile("/Users/vv/Documents/workii/2025MERN/index.html")
-})
 
-const PORT = 3001;
+
+app.get("/static", (req, res) => {
+  res.sendFile("/Users/vv/Documents/workii/2025MERN/index.html");
+});
+
+
+
+app.post("/signup", (req, res) => {
+  try {
+    const { firstName, lastName, phoneNumber, password, email } = req.body;
+    const newSignup = new Signup({
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+      password: password,
+      email: email,
+    });
+    
+    newSignup.save();
+    console.log("Signup Successful");
+    res.status(201).json({ message: "Signup Successful", isSignup: true });
+  } catch (error) {
+    console.log(error);
+    res.status(201).json({ message: "Signup Unsuccessful", isSignup: false });
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log("Server Started Successfully");
